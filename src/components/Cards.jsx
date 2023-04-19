dbb712bbc77002f17fa7ad60e696fc639d502dbe
Sarewell <libois-nicolas@hotmail.fr>
2023-04-19 13:48:23 +0200

    commit1

---
+++ src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import '../styles/style.css'
+import style from'./Card.module.css';
+import styles from '../styles/my-style.scss';
+function Card(){
+    return(
+        <div>
+            <h1 className='text-blue-500'>bonjour je suis toto </h1>
+            <p>je suis le maitre du monde</p>
+        </div>
+        
+    )
+}
+export default Card;
+// cree des element variable {}
